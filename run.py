efd34e6edddfaa2fba9980937f731a2715fa37c4
Zrufy <vincenzofornaro89@gmail.com>
2017-06-22 06:37:49 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response, render_template, request
+import json
+
+app = Flask(__name__)
+
+NAMES=["abc","abcd","abcde","abcdef"]
+
+@app.route('/autocomplete',methods=['GET'])
+def autocomplete():
+    search = request.args.get('autocomplete')
+    app.logger.debug(search)
+    return Response(json.dumps(NAMES), mimetype='application/json')
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    form = request.form.get("autocomp")
+    return render_template("search.html",form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
